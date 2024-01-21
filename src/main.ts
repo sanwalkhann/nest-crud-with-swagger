9c318afc57a4f112893a9c8b6885e01310a3a7ff
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2024-01-21 16:32:46 +0500

    done

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-/* eslint-disable prettier/prettier */
+  /* eslint-disable prettier/prettier */
 
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+  import { NestFactory } from '@nestjs/core';
+  import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+  import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Nest CRUD API')
-    .setDescription('API description')
-    .setVersion('1.0')
-    .addTag('api')
-    .build();
-  
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const config = new DocumentBuilder()
+      .setTitle('Nest CRUD API')
+      .setDescription('API description')
+      .setVersion('1.0')
+      .addTag('api')
+      .build();
+    
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
-}
+    await app.listen(3000);
+  }
 
-bootstrap();
+  bootstrap();
