478fc31398b8a14c77f8172e83d6fbc69d4eab2e
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2024-01-22 10:51:11 +0500

    swagger implemented

--- src/Schemas/user.schema.ts
+++ src/Schemas/user.schema.ts
@@ -1,23 +1,22 @@
 /* eslint-disable prettier/prettier */
-
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-
-
+import { ApiProperty } from '@nestjs/swagger';
 
 @Schema({
     timestamps: true,
 })
-
 export class User {
     @Prop()
+    @ApiProperty({ description: 'First name of the user', example: 'John' })
     firstname: string;
+
     @Prop()
+    @ApiProperty({ description: 'Last name of the user', example: 'Doe' })
     lastname: string;
-    @Prop({ unique: [true, 'Email already exist'] })
-    email: string
+
+    @Prop({ unique: [true, 'Email already exists'] })
+    @ApiProperty({ description: 'Email of the user', example: 'john.doe@example.com' })
+    email: string;
 }
 
-
-export const UserShema = SchemaFactory.createForClass(User)
-
-
+export const UserShema = SchemaFactory.createForClass(User);
--- src/user/DTO/create-user.dto.ts
+++ src/user/DTO/create-user.dto.ts
@@ -1,10 +1,14 @@
 /* eslint-disable prettier/prettier */
 
-
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'First name of the user', example: 'John' })
+  readonly firstname: string;
 
-    readonly firstname: string;
-    readonly lastname: string;
-    readonly email: string;
+  @ApiProperty({ description: 'Last name of the user', example: 'Doe' })
+  readonly lastname: string;
+
+  @ApiProperty({ description: 'Email of the user', example: 'john.doe@example.com' })
+  readonly email: string;
 }
